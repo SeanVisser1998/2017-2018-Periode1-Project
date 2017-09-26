d009c90e6e9d0859fc1399dfcb4a6e5af15aee92
SeanVisser1998 <32290279+SeanVisser1998@users.noreply.github.com>
2017-09-26 09:31:44 +0200

    Login
    login

--- periode_1/inc/login/login.inc.php
+++ periode_1/inc/login/login.inc.php
@@ -5,15 +5,37 @@
 	* Project: Webdevelopment
 	**/
 
+	//Include de algemene SQL configuratie
+
 
 
 	//Functie voor het ophalen van de inlog pagina 
 	//CSS naam: login
+	//https://www.formget.com/login-form-in-php/
 
 	function getLoginPage(){
 
 		echo "<h1>Login Pagina</h1>";
 		echo "<p1>Dit is de login pagina<p1>";
+		echo "<div id=\"login_form\">"; //ID VOOR CSS: login_form
+		echo "<form action=\"\" method=\"post\">";
+		echo "<label>Gebruikersnaam: </label>";
+		echo "<input id=\"name\" name=\"username\" placeholder=\"username\" type=\"text\">";
+		echo "<input id=\"password\" name=\"password\" placeholder=\"***********\" type=\"password\">";
+		echo "<input name\"invoeren\" type=\"submit\" value=\" Login \">";
+
+
+	}
+
+	//Functie voor het nagaan van het profiel
+	function getProfile(){
+
+
+	}
+
+	//Functie voor het maken van een session, wordt gebruik gemaakt van MySQL verbinden.
+	function getSession(){
+
 
 	}
 
